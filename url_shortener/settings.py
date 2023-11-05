79ac67d5aae5081262bc8f73d3ad0829df9f3e9c
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-05 18:23:21 +0200

    update requirements

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -151,7 +151,7 @@ SHORTCODE_MIN = 6
 SHORTCODE_MAX = 15
 SHORTENER_QR_CODE_DIR = 'shortener/qr_codes'
 # CSRF_TRUSTED_ORIGINS = ['https://ngrok']
-
+URL = 'http://127.0.0.1:8000'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
